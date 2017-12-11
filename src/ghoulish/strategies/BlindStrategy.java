c71f3ced2e9032a47d5691a06b8ee85650a009e2
Svistoplyas <danilin96@gmail.com>
2017-12-11 13:00:15 +0700

    Strategy started

---
+++ src/ghoulish/strategies/BlindStrategy.java
@@ -0,0 +1,14 @@
+package ghoulish.strategies;
+
+import ghoulish.creatures.Monster;
+import javafx.util.Pair;
+
+/**
+ * Created by Alexandr on 11.12.2017.
+ */
+public class BlindStrategy implements IStrategy {
+    @Override
+    public Pair<Integer, Integer> move(Monster monster) {
+        return null;
+    }
+}
